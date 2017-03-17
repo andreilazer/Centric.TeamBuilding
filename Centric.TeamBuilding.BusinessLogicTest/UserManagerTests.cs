d91e67fe423a2fc93641e36eeaa911f2d633964c
andrei <andrei.lazar@centric.eu>
2017-03-17 19:40:56 +0200

    Added business logic test

---
+++ Centric.TeamBuilding.BusinessLogicTest/UserManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Centric.TeamBuilding.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Centric.TeamBuilding.BusinessLogicTest
+{
+    [TestClass]
+    public class UserManagerTests
+    {
+        [TestMethod]
+        public void RegisterUser_ShouldThrowInvalidException_WhenEmptyRequiredFields()
+        {
+            var user = new User
+            {
+
+            };
+        }
+    }
+}
